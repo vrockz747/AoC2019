178e1f446b42f66ac7300205dd0f938523105746
Vaibhav Sethia <vrockz747@gmail.com>
2025-02-16 23:24:43 +0530

    Add uthash, day three part one complete

--- 3/aoc3.c
+++ 3/aoc3.c
@@ -1,87 +1,64 @@
+#include "../uthash.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-#define ROW_LEN 40000
-#define COL_LEN 40000
 typedef struct {
   int x;
   int y;
 } point;
+typedef struct {
+  point cord;
+  char value;
+  UT_hash_handle hh;
+} map;
 
-void set_mark(char *current, char set) {
-  if (*current == set) {
-    return;
-  }
-  if (*current != 0) {
-    // cross
-    *current = 'X';
-  }
-}
+map *mmap = NULL;
+int sum = 0;
 
-void trace_path_x(int num, point *p_point, char grid[][ROW_LEN], char mark) {
-  if (p_point->x + num >= ROW_LEN) {
-    printf("pointx%d num%d\n", p_point->x, num);
-    printf("Row OOB\n");
-    exit(-1);
-  }
-
-  if (num < 0) {
-    for (int i = -1; i >= num; i--) {
-      set_mark(&grid[p_point->y][p_point->x + i], mark);
+void update_map(point current, char marker) {
+  map *out = NULL;
+  HASH_FIND(hh, mmap, &current, sizeof(current), out);
+  if (out) {
+    // check with marker
+    if (out->value != marker) {
+      // if marker different make it X and store x + y sum
+      out->value = 'X';
+      int temp = abs(current.x) + abs(current.y);
+      if (temp < sum || sum == 0) {
+        sum = temp;
+      }
     }
   } else {
-    for (int i = 1; i <= num; i++) {
-      set_mark(&grid[p_point->y][p_point->x + i], mark);
-    }
+    // mark the cords
+    out = (map *)malloc(sizeof(map));
+    out->cord = current;
+    out->value = marker;
+    HASH_ADD(hh, mmap, cord, sizeof(point), out);
   }
-  p_point->x += num;
 }
 
-void trace_path_y(int num, point *p_point, char grid[][ROW_LEN], char mark) {
-  if (p_point->y + num >= COL_LEN) {
-    printf("pointx%d num%d\n", p_point->y, num);
-
-    printf("COL OOB\n");
-    exit(-1);
+void trace_path(int x, int y, char marker, int offset, point *prev_point) {
+  for (int i = 0; i < offset; i++) {
+    prev_point->x += x;
+    prev_point->y += y;
+    update_map(*prev_point, marker);
   }
-  if (num < 0) {
-    for (int i = -1; i >= num; i--) {
-      set_mark(&grid[p_point->y + i][p_point->x], mark);
-    }
-  } else {
-    for (int i = 1; i <= num; i++) {
-      set_mark(&grid[p_point->y + i][p_point->x], mark);
-    }
-  }
-
-  p_point->y += num;
 }
 
-void update_grid(char grid[][ROW_LEN], char direction, int num, char mark) {
-  static point prev_point = {COL_LEN / 2, ROW_LEN / 2};
-  switch (direction) {
-  case 'L': {
-    trace_path_x(num * (-1), &prev_point, grid, mark);
-
-  } break;
-
-  case 'R': {
-    trace_path_x(num, &prev_point, grid, mark);
-
-  } break;
-
-  case 'U': {
-    trace_path_y(num, &prev_point, grid, mark);
-
-  } break;
-
-  case 'D': {
-    trace_path_y(num * (-1), &prev_point, grid, mark);
-
-  } break;
-
-  default:
+void update_grid(int offset, char dir, char marker, point *prev_point) {
+  // get dir and add cords to mmap
+  switch (dir) {
+  case 'L':
+    trace_path(-1, 0, marker, offset, prev_point);
+    break;
+  case 'R':
+    trace_path(1, 0, marker, offset, prev_point);
+    break;
+  case 'U':
+    trace_path(0, 1, marker, offset, prev_point);
+    break;
+  case 'D':
+    trace_path(0, -1, marker, offset, prev_point);
     break;
   }
 }
@@ -89,26 +66,19 @@ void update_grid(char grid[][ROW_LEN], char direction, int num, char mark) {
 int main() {
   FILE *file = fopen("input.txt", "r");
   char buffer[2000];
-  static char grid[COL_LEN][ROW_LEN] = {0};
+
+  int marker = 0;
   while (fgets(buffer, sizeof(buffer), file)) {
-    int mark = 0;
     char *token = strtok(buffer, ",");
-    // write to grid
+    point prev_point = {0, 0};
     while (token) {
+      int offset;
       char dir;
-      int num;
-      sscanf(token, "%c%d", &dir, &num);
-      update_grid(grid, dir, num, 'A' + mark);
+      sscanf(token, "%c%d", &dir, &offset);
+      update_grid(offset, dir, 'A' + marker, &prev_point);
       token = strtok(NULL, ",");
     }
-    // update mark
-    ++mark;
+    marker++;
   }
-
-  for (int i = ROW_LEN/2; i < ROW_LEN; i++) {
-    for (int j = ROW_LEN/2; j < ROW_LEN; j++) {
-      printf("%c", grid[i][j]);
-    }
-    printf("\n");
-  }
+  printf("SUM: %d\n", sum);
 }
