04e1d5985d539445d7d691a2c31437feda80f097
Vaibhav Sethia <vrockz747@gmail.com>
2025-03-09 03:35:34 +0530

    day three part two complete

--- 3/aoc3.c
+++ 3/aoc3.c
@@ -1,4 +1,5 @@
 #include "../uthash.h"
+#include <limits.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -9,13 +10,16 @@ typedef struct {
 typedef struct {
   point cord;
   char value;
+  int steps;
   UT_hash_handle hh;
 } map;
 
+// understand the TIME COMP
 map *mmap = NULL;
 int sum = 0;
+int sum_steps = INT_MAX;
 
-void update_map(point current, char marker) {
+void update_map(point current, char marker, int *steps) {
   map *out = NULL;
   HASH_FIND(hh, mmap, &current, sizeof(current), out);
   if (out) {
@@ -27,38 +31,47 @@ void update_map(point current, char marker) {
       if (temp < sum || sum == 0) {
         sum = temp;
       }
+      int local = (out->steps + (++(*steps)));
+      printf("%d\n",local);
+
+      if(sum_steps > local){
+        sum_steps = local;
+      }
+    }else{
+      (*steps)++;
     }
   } else {
     // mark the cords
     out = (map *)malloc(sizeof(map));
+    out->steps = ++(*steps);
     out->cord = current;
     out->value = marker;
     HASH_ADD(hh, mmap, cord, sizeof(point), out);
   }
 }
 
-void trace_path(int x, int y, char marker, int offset, point *prev_point) {
+void trace_path(int x, int y, char marker, int offset, point *prev_point, int *steps) {
   for (int i = 0; i < offset; i++) {
     prev_point->x += x;
     prev_point->y += y;
-    update_map(*prev_point, marker);
+    update_map(*prev_point, marker, steps);
   }
 }
 
-void update_grid(int offset, char dir, char marker, point *prev_point) {
+void update_grid(int offset, char dir, char marker, point *prev_point, int *steps) {
   // get dir and add cords to mmap
   switch (dir) {
   case 'L':
-    trace_path(-1, 0, marker, offset, prev_point);
+    trace_path(-1, 0, marker, offset, prev_point, steps);
     break;
   case 'R':
-    trace_path(1, 0, marker, offset, prev_point);
+    trace_path(1, 0, marker, offset, prev_point, steps);
     break;
   case 'U':
-    trace_path(0, 1, marker, offset, prev_point);
+    trace_path(0, 1, marker, offset, prev_point, steps);
     break;
   case 'D':
-    trace_path(0, -1, marker, offset, prev_point);
+    trace_path(0, -1, marker, offset, prev_point, steps);
     break;
   }
 }
@@ -68,6 +81,7 @@ int main() {
   char buffer[2000];
 
   int marker = 0;
+  int steps = 0;
   while (fgets(buffer, sizeof(buffer), file)) {
     char *token = strtok(buffer, ",");
     point prev_point = {0, 0};
@@ -75,10 +89,13 @@ int main() {
       int offset;
       char dir;
       sscanf(token, "%c%d", &dir, &offset);
-      update_grid(offset, dir, 'A' + marker, &prev_point);
+      update_grid(offset, dir, 'A' + marker, &prev_point, &steps);
       token = strtok(NULL, ",");
     }
+    steps = 0;
     marker++;
   }
   printf("SUM: %d\n", sum);
+    printf("STEPS: %d\n", sum_steps);
+
 }
